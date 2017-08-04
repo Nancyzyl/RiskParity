67a1ac6c14eaa8ffc9ff4a69d6044804487be691
Nancyzyl <zyluo@ruc.edu.cn>
2017-08-04 15:58:46 +0800

    增加交易成本

--- VolParity.R
+++ VolParity.R
@@ -13,13 +13,13 @@ logreturn <- logreturn[-1, ] - logreturn[-(nrow(logreturn)), ]
 # logreturn <- logreturn[, -1]
 
 riskbudget <- read.csv('riskbudget.csv')
-riskbudget <- riskbudget[, 2]
+riskbudget <- riskbudget[, 1]
 # riskbudget <- read.csv('risk2.csv')
 # riskbudget <- drop(as.matrix(riskbudget))
 parameter <- read.csv('ParaVol.csv')
 
 perform <- NULL
-VolParity <- function(logdata, riskbudget, parameter){
+VolParity <- function(logdata, riskbudget, parameter, costs = rep(0.0001, 40)){
   attach(parameter)
   data <- exp(logdata) - 1
   trade.date <- rownames(data)
@@ -35,7 +35,7 @@ VolParity <- function(logdata, riskbudget, parameter){
   }
   signal.adjust <- SignalAdjust(signal = signal)
   for(i in time.period3:nrow(logdata)){
-    optweight <- VolWeight(data = logdata, date.calculate = trade.date[i], days.vol)
+    optweight <- VolWeight(data = logdata, date.calculate = trade.date[i], days.vol) 
    # weight.ts <- rbind(weight.ts, optweight)
     weight.ts <- rbind(weight.ts, VolatilityMonitor(data = data,
                                                    date.calculate = trade.date[i],
@@ -47,22 +47,22 @@ VolParity <- function(logdata, riskbudget, parameter){
   rownames(signal.adjust) <- rownames(weight.ts) <- trade.date[time.period3:nrow(logdata)]
   #weight.temp <- signal.adjust * weight.ts
   #finalweight <- t(apply(weight.temp, 1, function(x) { x * riskbudget}))
-  finalweight <- weight.ts
-  returns <- rowSums((as.matrix(data[((time.period3 + 1):nrow(data)), ]) * finalweight[-nrow(finalweight), ]))
+  returns <- rowSums((as.matrix(data[((time.period3 + 1):nrow(data)), ] - TradeCost(weight.ts, catecosts = costs)) * weight.ts[-nrow(weight.ts), ]))
   performance <- PerformEva(returns)
   detach(parameter)
-  return(list(signal = signal, signal.adjust = signal.adjust, weight.ts = weight.ts, finalweight = finalweight,
+  return(list(signal = signal, signal.adjust = signal.adjust, weight.ts = weight.ts, 
               returns = returns, performance = performance))
 }
+
 for(k in 1:nrow(parameter)){
-  ans <- VolParity(logreturn, riskbudget, parameter[k, ])
-  # logsum <- cumsum(log(ans$returns + 1))
-  # plot(logsum, type = 'l')
-  # write.csv(ans$signal.adjust, 'signal1.csv')
-  # write.csv(ans$weight.ts, 'weight.ts1.csv')
-  # write.csv(ans$finalweight,'finalweight1.csv')
-  # write.csv(ans$returns,'returns1.csv')
-  # plot(ans$performance$net.value, type = 'l')
+  ans <- VolParity(logdata = logreturn, riskbudget = riskbudget, parameter = parameter[k, ])
+  logsum <- cumsum(log(ans$returns + 1))
+  plot(logsum, type = 'l')
+  write.csv(ans$signal.adjust, paste('signal1_', k, '.csv'))
+  write.csv(ans$weight.ts, 'weight.ts1.csv')
+  write.csv(ans$finalweight,'finalweight1.csv')
+  write.csv(ans$returns,'returns1.csv')
+  plot(ans$performance$net.value, type = 'l')
   perform <- rbind(perform, ans$performance[-(1:2)])
 }
 
